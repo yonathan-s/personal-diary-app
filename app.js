5dc08b407efedfca80c924614a9a519ba65fafaa
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-08-28 13:00:20 +0100

    Crud complete and start on views

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+
 const app = express()
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 import diaryRouter from './routers/diary.js'
+
 
 app.use(cors())
 app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.json({
-        name: "Diary",
-        description: "An app where you can write diary entries"
-    })
+    res.render('index')
 })
+
+
 
 app.use('/entries', diaryRouter)
 
